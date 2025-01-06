6a9d54888e438190e8eecf8d5ed1c43a5429e0e8
aamensah <aamensah@quantumgroupgh.com>
2025-01-06 15:31:04 +0000

    add nwg link to app bar

--- app/components/AppBar.vue
+++ app/components/AppBar.vue
@@ -1,11 +1,14 @@
 <template>
   <v-app-bar flat class="bg-newgas">
     <template v-slot:prepend>
+      <a href="https://newgas.online/">
       <v-img src="~/assets/NewgasFinalLogo.png" width="30px" height="30px"></v-img>
+    </a>
     </template>
-
     <v-app-bar-title class="font-weight-regular">
+      <a class="text-white" style="text-decoration: none;" href="https://newgas.online/">
       NEWGAS
+    </a>
     </v-app-bar-title>
   </v-app-bar>
 </template>
